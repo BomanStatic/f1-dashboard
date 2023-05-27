730b3234f33fe590ed5986ff996d04bd8e37d728
BomanStatic <boman.staticsites@gmail.com>
2023-05-27 11:27:16 +0200

    Fixed: img url

---
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/3.js
@@ -0,0 +1,9 @@
+import * as universal from '../entries/pages/_driverId_/_page.js';
+
+export const index = 3;
+export const component = async () => (await import('../entries/pages/_driverId_/_page.svelte.js')).default;
+export { universal };
+export const universal_id = "src/routes/[driverId]/+page.js";
+export const imports = ["_app/immutable/nodes/3.ab4e8929.js","_app/immutable/chunks/index.82199ac0.js","_app/immutable/chunks/index.c1f1f2b7.js"];
+export const stylesheets = ["_app/immutable/assets/3.28e5a062.css"];
+export const fonts = [];
---
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -0,0 +1,35 @@
+export const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png","img/Albon.webp","img/Alonso.webp","img/Bottas.webp","img/Circuit/Monte-Carlo.webp","img/Circuit/Montmeló.webp","img/de Vries.webp","img/Gasly.webp","img/Hamilton.webp","img/Hülkenberg.webp","img/Leclerc.webp","img/Magnussen.webp","img/Norris.webp","img/Ocon.webp","img/Piastri.webp","img/Pérez.webp","img/Russell.webp","img/Sainz.webp","img/Sargeant.webp","img/Stroll.webp","img/Tsunoda.webp","img/Verstappen.webp","img/Zhou.webp"]),
+	mimeTypes: {".png":"image/png",".webp":"image/webp"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.cb06070f.js","app":"_app/immutable/entry/app.44fbf6d2.js","imports":["_app/immutable/entry/start.cb06070f.js","_app/immutable/chunks/index.82199ac0.js","_app/immutable/chunks/singletons.c04db461.js","_app/immutable/entry/app.44fbf6d2.js","_app/immutable/chunks/index.82199ac0.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('../output/server/nodes/0.js'),
+			() => import('../output/server/nodes/1.js'),
+			() => import('../output/server/nodes/2.js'),
+			() => import('../output/server/nodes/3.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			},
+			{
+				id: "/[driverId]",
+				pattern: /^\/([^/]+?)\/?$/,
+				params: [{"name":"driverId","optional":false,"rest":false,"chained":false}],
+				page: { layouts: [0,], errors: [1,], leaf: 3 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
