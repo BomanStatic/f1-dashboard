790cc9d6d9077421651a316ab1a859ebef273f61
BomanStatic <133013695+BomanStatic@users.noreply.github.com>
2023-05-27 11:30:56 +0200

    Delete .vercel/output directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.7df9d612.js","_app/immutable/chunks/index.82199ac0.js","_app/immutable/chunks/singletons.c04db461.js"];
-export const stylesheets = [];
-export const fonts = [];
